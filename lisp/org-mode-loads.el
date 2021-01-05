d45ae47a1582a189667c0b8e91da513294e9a908
Lucas Gen <lg08@pop-os.localdomain>
2021-01-05 12:13:09 -0800

    adds agenda thing

--- lisp/org-mode-loads.el
+++ lisp/org-mode-loads.el
@@ -6,33 +6,6 @@
 
   )
 
-(use-package org
-  :defer t
-  ;; :hook (org-mode . efs/org-mode-setup)
-  :config
-  (setq org-ellipsis " ▾")
-
-  (setq org-agenda-start-with-log-mode t)
-  (setq org-log-done 'time)
-  (setq org-log-into-drawer t)
-
-  ;; (setq org-agenda-files
-  ;;       '("~/Projects/Code/emacs-from-scratch/OrgFiles/Tasks.org"
-  ;;         "~/Projects/Code/emacs-from-scratch/OrgFiles/Habits.org"
-  ;;         "~/Projects/Code/emacs-from-scratch/OrgFiles/Birthdays.org"))
-
-  (require 'org-habit)
-  (add-to-list 'org-modules 'org-habit)
-  (setq org-habit-graph-column 60)
-
-  (setq org-todo-keywords
-        '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
-          (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
-
-  (setq org-refile-targets
-        '(("Archive.org" :maxlevel . 1)
-          ("Tasks.org" :maxlevel . 1)))
-  )
 (use-package org-bullets                ;shows prettier bullets in org mode
   :defer t
   :config
@@ -45,6 +18,8 @@
   (setq TeX-parse-self t)
   (setq-default TeX-master nil)
   )
+
+(setq org-agenda-files (list "~/.emacs.d/agenda.org"))
 
 
 (provide 'org-mode-loads)
